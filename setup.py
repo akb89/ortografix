d834442d6902f3ff9ebf6f7d5ee722b8be56f9df
AKB <akb@3azouz.net>
2020-05-12 20:24:49 +0200

    bugfix added missing MANIFEST.in to include logging.yml file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='akb@3azouz.net',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.3.0',
+    version='0.3.1',
     url='https://github.com/akb89/ortografix',
     download_url='https://github.com/akb89/ortografix',
     license='MIT',
